897a26f140264576a6b88f918f8273635fb0e354
Bhanuka Rajakaruna <87462362+BhanukaRajk@users.noreply.github.com>
2023-06-26 23:27:04 +0530

    Comments added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,34 @@
+import { useState } from "react"
 import "./styles.css"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [tasks, setTasks] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTasks((currentTask) => {
+      return [...currentTask,
+        { id: crypto.randomUUID(), title: newItem, completed: false }]
+    })
+    setTasks((currentTask) => {
+      return [...currentTask,
+        { id: crypto.randomUUID(), title: newItem, completed: false }]
+    })
+
+    console.log(tasks)
+  }
+
   return (
     // USE <> OR <div> TO USE THIS ALL AS AN ONE SET. IN HERE <> IS USED.
     // BECAUSE <h1> AND <form> ARE TWO SETS
     <>
       <h1 className="heading-one">Hi</h1>
-      <form className="Task-adding-form">
+      <form onSubmit={handleSubmit} className="Task-adding-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input type="text" id="item" />
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
         </div>
         <button className="btn">Add</button>
       </form>
